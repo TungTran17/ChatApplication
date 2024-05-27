94faa7fb5148e9b9aab6598ae9c33075958b4d16
TungTran17 <tungtvhe163636@fpt.edu.vn>
2024-05-28 02:24:38 +0700

    update auth

--- app/src/main/java/com/example/chatapplication/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapplication/activities/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatapplication.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +10,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.chatapplication.R;
+import com.example.chatapplication.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
     }
 }
